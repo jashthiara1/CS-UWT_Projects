cb2a4a8b91dc7ee3325f43c2960da7b0637c60f9
Jash Thiara <52906018+jashthiara1@users.noreply.github.com>
2024-11-18 11:55:43 -0800

    Delete Student-RealEstate directory

--- Student-RealEstate/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DBHOST', 'localhost');
-define('DBNAME', 'company');
-define('DBUSER', 'testuser');
-define('DBPASS', 'mypassword');
-define('DBCONNSTRING','mysql:dbname=company;charset=utf8mb4');
-?>
