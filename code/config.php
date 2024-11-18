1a14074827db2f9042f22c6387d98e92e75e20a4
Jash Thiara <52906018+jashthiara1@users.noreply.github.com>
2024-11-18 12:05:37 -0800

    Delete code directory

--- code/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DBHOST', 'localhost');
-define('DBNAME', 'StudentRealEstate');
-define('DBUSER', 'testuser');
-define('DBPASS', 'mypassword');
-define('DBCONNSTRING','mysql:dbname=StudentRealEstate;charset=utf8mb4');
-?>
